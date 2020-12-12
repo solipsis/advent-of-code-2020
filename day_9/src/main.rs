b699a5ec1072eb052a1626a8ecf9ce0ac85926fc
solipsis <davida3141@gmail.com>
2020-12-11 21:01:27 -0700

    parts 1 + 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,16 +1,14 @@
 use std::collections::HashSet;
 
-const PREAMBLE_SIZE: usize = 5;
+const PREAMBLE_SIZE: usize = 25;
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let vals: Vec<usize> = input
         .lines()
         .map(|x| x.parse().unwrap())
         .collect();
-    println!("{:?}", vals);
 
-
-   // populate_targets(5, &vals, &mut targets);
+    let mut result: usize = 0;
 
     for idx in PREAMBLE_SIZE..vals.len() {
         let mut valid = false;
@@ -25,21 +23,44 @@ fn main() {
         }
         if !valid {
             println!("Done: {}", vals[idx]);
-            return
+            result = vals[idx];
+            break;
         }
     }
 
+    part2(&vals, result);
 }
 
 fn populate_targets(idx: usize, input: &Vec<usize>, targets: &mut HashSet<usize>) {
     let target = &input[idx];
     let input = &input[idx-PREAMBLE_SIZE..idx+1];
-    println!("{:?}", input);
-    println!("input IDX: {}", target);
-    
+
     for i in 0..PREAMBLE_SIZE {
         targets.insert(target.wrapping_sub(input[i]));
     }
+}
 
-    println!("{:?}", targets);
+fn part2(input: &Vec<usize>, target: usize) {
+    let mut tail_idx: usize = 0;
+    let mut head_idx: usize = 0;
+    let mut sum: usize = 0;
+
+    loop {
+        if sum < target {
+            sum += input[head_idx];
+            head_idx += 1;
+        } else if sum > target {
+            sum -= input[tail_idx];
+            tail_idx += 1;
+        }
+
+        if sum == target {
+            let range = &input[tail_idx..head_idx-1];
+            let min = range.iter().min().unwrap();
+            let max = range.iter().max().unwrap();
+
+            println!("Part2: {}", min + max);
+            return
+        }
+    }
 }

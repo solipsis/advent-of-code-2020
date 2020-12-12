28f991933149bd2c3c8140426a2949b18ebec2bf
solipsis <davida3141@gmail.com>
2020-12-12 14:25:35 -0700

    day 10 part2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let mut input: Vec<i32> = std::fs::read_to_string("input.txt").unwrap()
+    let mut input: Vec<u64> = std::fs::read_to_string("input.txt").unwrap()
         .lines()
         .map(|x| x.parse().unwrap())
         .collect();
 
     input.sort();
-    println!("{:?}", input);
 
     let mut prev = 0;
     let mut one_count = 0;
@@ -21,33 +20,24 @@ fn main() {
     }
     three_count += 1;
 
-    println!("1: {}, 3: {}", one_count, three_count);
-    println!("Product: {}", one_count*three_count);
+    println!("Part1: {}", one_count*three_count);
     part2(&input);
 }
 
-fn part2(input: &Vec<i32>) {
-    let mut idx = input.len()-1;
-    let mut acc: u64 = 1;
-    while idx > 0 {
-        let mut sub_idx = idx-1;
-        let mut ways = 0;
-        while  input[sub_idx]+3 >= input[idx] {
-            ways += 1;
-            if sub_idx == 0 {
-                break;
-            }
-            sub_idx -= 1;
-        }
-        println!("val: {}, ways: {}", input[idx], ways);
-        match ways {
-            2 => acc *= 2,
-            3 => acc *= 4,
-            _ => (),
-        }
-        idx -= ways;
-    }
-    println!("Acc: {}", acc);
+fn part2(input: &Vec<u64>) {
+    let memo_size = (*input.iter().max().unwrap() + 3) as usize;
+    let mut idx = 0;
+    let mut memo: Vec<u64> = vec![0; memo_size];
 
+    // initialize first dp value
+    memo[2] = 1;
+
+    while idx < input.len() {
+        let v: usize = input[idx] as usize; // val 1 should be in memo[3]
+        memo[v+2] = memo[v+1] + memo[v] + memo[v-1];
+        idx += 1;
+    }
+
+    println!("Part2: {}", memo.iter().max().unwrap());
 }
 

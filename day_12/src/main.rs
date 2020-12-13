8ae75cb9e8cfca4e9d01953c5b932c877bc38c51
solipsis <davida3141@gmail.com>
2020-12-12 22:02:47 -0700

    day_12 part 2

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -21,7 +21,6 @@ fn main() {
 
     let dirs: Vec<(i32, i32)> = vec![(1,0),(0,-1),(-1,0),(0,1)];
     let mut direction_idx = 0;
-    //let mut direction: (i32, i32) = dirs[directio];
 
     
     let mut cmds: Vec<Command> = Vec::new();
@@ -42,7 +41,7 @@ fn main() {
         cmds.push(cmd);
     }
 
-    for cmd in cmds {
+    for cmd in &cmds {
         match cmd {
             Command::Right(deg) => direction_idx = ((deg/90) + direction_idx) % 4,
             Command::Left(deg) => direction_idx = (4 - (deg/90) + direction_idx) % 4,
@@ -56,10 +55,64 @@ fn main() {
             Command::South(v) => y -= v,
             _ => println!("FAIL"),
         }
-        println!("cmd: {:?}", cmd);
-        println!("x: {}, y: {}", x, y);
     }
     println!("x: {}, y: {}", x, y);
     println!("Part 1: {}", x.abs() + y.abs());
 
+    part2(&cmds);
+}
+
+fn part2(cmds: &Vec<Command>) {
+    
+    let mut x = 0;
+    let mut y = 0;
+    let mut way_x = 10;
+    let mut way_y = 1;
+
+    for cmd in cmds {
+        match cmd {
+            Command::Right(deg) => {
+                let rot = rot_right(deg/90, way_x, way_y);
+                way_x = rot.0;
+                way_y = rot.1;
+            },
+            Command::Left(deg) => {
+                let rot = rot_left(deg/90, way_x, way_y);
+                way_x = rot.0;
+                way_y = rot.1;
+            },
+            Command::Forward(v) => {
+                x += v * way_x;
+                y += v * way_y;
+            },
+            Command::East(v) => way_x += v,
+            Command::West(v) => way_x -= v,
+            Command::North(v) => way_y += v,
+            Command::South(v) => way_y -= v,
+            _ => println!("FAIL"),
+        }
+    }
+    println!("Part 2: {}", x.abs() + y.abs());
+}
+
+fn rot_right(mut times: i32, mut x: i32, mut y: i32) -> (i32, i32) {
+    while times > 0 {
+        let tmp = x;
+        x = y;
+        y = tmp;
+        y *= -1;
+        times -= 1;
+    }
+    return (x, y);
+}
+
+fn rot_left(mut times: i32, mut x: i32, mut y: i32) -> (i32, i32) {
+    while times > 0 {
+        let tmp = x;
+        x = y;
+        y = tmp;
+        x *= -1;
+        times -= 1;
+    }
+    return (x, y);
 }

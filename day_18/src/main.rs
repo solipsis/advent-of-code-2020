82eca07beb3e49fc4d790d0fe89b97a6e935c304
solipsis <davida3141@gmail.com>
2020-12-18 10:54:43 -0700

    finally get ownership right

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,19 +1,33 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let mut sum: isize = 0;
+    let mut sum_2: isize = 0;
 
     for line in input.lines() {
-        let v = calc(line.to_string());
-        sum += v;
-        println!("line_done: {}", v);
+        sum += calc(line.to_string());
+
+        // for part 2, inject parens around each terms before and after each plus sign
+        let terms: Vec<&str> = line.split(" ").collect();
+        let mut replaced: Vec<String> = terms.clone().iter().map(|x| x.to_string()).collect();
+        for (idx, term) in terms.iter().enumerate() {
+            //println!("term: {}", term);
+            if term.contains("+") {
+                replaced[idx-1].insert_str(0, "(");
+                replaced[idx+1].push_str(")");
+             //   println!("After replace: {} {}", replaced[idx-1], replaced[idx+1])
+            }
+        }
+        //let v2 = calc(replaced.join(" "));
+        sum_2 += calc(replaced.join(" "));
     }
 
     println!("Part1: {}", sum);
+    println!("Part2: {}", sum_2);
 }
 
 fn calc(eq: String) -> isize {
+    //println!("original: {}", eq);
     let mut eq: String = eq.clone();
-    let mut val: isize = -1;
     // repeatedly find rightmost lparen
     // calc until r paren
     // replace with vals, remove parens, repeat.
@@ -23,7 +37,7 @@ fn calc(eq: String) -> isize {
             Some(idx) => {
                 let sub = &eq[idx..];
                 let rparen = sub.find(')').unwrap(); // assume exists?
-                val = solve(sub[1..rparen].to_string());
+                let val = solve(sub[1..rparen].to_string());
                 eq.replace_range(idx..(idx+rparen+1), &val.to_string());
             }
             None => return solve(eq),
@@ -34,7 +48,7 @@ fn calc(eq: String) -> isize {
 }
 
 fn solve(eq: String) -> isize {
-    println!("sub: {}", eq);
+    //println!("sub: {}", eq);
     let terms: Vec<&str> = eq.split(" ").collect();
     let mut acc: isize = terms[0].parse().unwrap();
 
@@ -53,6 +67,6 @@ fn solve(eq: String) -> isize {
             }
         }
     }
-    println!("res: {}", acc);
+    //println!("res: {}", acc);
     return acc;
 }

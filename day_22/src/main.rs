b3f088e55e95e09537e9f19b0a66ea785d0996a3
solipsis <davida3141@gmail.com>
2020-12-22 11:51:49 -0700

    refactor and massive speedup for part 2

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::VecDeque;
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn main() {
@@ -10,57 +9,41 @@ fn main() {
         .map(|x| x.to_owned())
         .collect();
     
-    let p1: VecDeque<usize> = input[0].split("\n").skip(1).map(|x| x.parse().unwrap()).collect();
-    let p2: VecDeque<usize> = input[1].split("\n").skip(1).map(|x| x.parse().unwrap()).collect();
-    let mut memo: HashMap<String, bool> = HashMap::new();
+    let p1: VecDeque<u8> = input[0].split("\n").skip(1).map(|x| x.parse().unwrap()).collect();
+    let p2: VecDeque<u8> = input[1].split("\n").skip(1).map(|x| x.parse().unwrap()).collect();
 
     part_1(p1.clone(), p2.clone());
-
-    recursive_game(p1, p2, &mut memo, 1);
-
+    recursive_game(p1, p2, 1);
 }
 
-fn recursive_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, memo: &mut HashMap<String,bool>,  game: usize) -> bool {
-//    println!("start game: {}", game);
+fn recursive_game(mut p1: VecDeque<u8>, mut p2: VecDeque<u8>,  game: usize) -> bool {
     
     // track all seen states in this particular game
-    let mut states: HashSet<String> = HashSet::new();
+    let mut states_p1: HashSet<VecDeque<u8>> = HashSet::new();
+    let mut states_p2: HashSet<VecDeque<u8>> = HashSet::new();
     let mut sub_game = game;
 
-    // hash the state to see if we have seen this substate before
-    let hash = blake3::hash(to_id(&p1, &p2).as_bytes()).to_hex().to_string();
-    if let Some(prev) = memo.get(&hash) {
-       // println!("hooray-cache?: {}", memo.len());
-    //    println!("cached game {} done: {}, cache-size: {}", game, *prev, memo.len());
-        return *prev;
-    }
-
-    //println!("{}", hash);
-
     while p1.len() > 0 && p2.len() > 0 {
-        let hash = blake3::hash(to_id(&p1, &p2).as_bytes()).to_hex().to_string();
-        if states.contains(&hash) {
+
+        if states_p1.contains(&p1) || states_p2.contains(&p2) {
             return true; // player one wins due to anti-loop rule
         }
-        states.insert(hash.clone());
+        states_p1.insert(p1.clone());
+        states_p2.insert(p2.clone());
 
         let c1 = p1.pop_front().unwrap();
         let c2 = p2.pop_front().unwrap();
 
         // recursive combat
-        if p1.len() >= c1 && p2.len() >= c2 {
+        if p1.len() >= c1 as usize && p2.len() >= c2 as usize {
             // create new decks that are copies
-            let mut nd1: VecDeque<usize> = p1.clone();
-            while nd1.len() != c1 {
-                nd1.pop_back();
-            }
-            let mut nd2: VecDeque<usize> = p2.clone();
-            while nd2.len() != c2 {
-                nd2.pop_back();
-            }
+            let mut nd1: VecDeque<u8> = p1.clone();
+            nd1.truncate(c1 as usize);
+            let mut nd2: VecDeque<u8> = p2.clone();
+            nd2.truncate(c2 as usize);
             // p1 wins if true
             sub_game += 1;
-            if recursive_game(nd1.clone(), nd2.clone(), memo, sub_game) {
+            if recursive_game(nd1, nd2, sub_game) {
                 p1.push_back(c1);
                 p1.push_back(c2);
             } else {
@@ -80,37 +63,30 @@ fn recursive_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, memo: &mut H
     let p1_wins = if p1.len() > p2.len() {true} else {false};
 
     if game == 1 {
-        let mut score = 0;
-        let mut multiplier = 1;
-        while p1.len() > 0 {
-            score += p1.pop_back().unwrap() * multiplier;
-            multiplier += 1;
-        }
-        while p2.len() > 0 {
-            score += p2.pop_back().unwrap() * multiplier;
-            multiplier += 1;
-        }
-        println!("Part 2: {}", score);
+        println!("Part 2: {}", score(&p1, &p2));
     }
 
-    // memoize
-    memo.insert(hash, p1_wins);
     return p1_wins;
 }
 
-fn to_id(p1: &VecDeque<usize>, p2: &VecDeque<usize>) -> String {
-    let mut elements: Vec<String> = vec![String::from("p1")];
-    for el in p1 {
-        elements.push(el.to_string());
+fn score(p1: &VecDeque<u8>, p2: &VecDeque<u8>) -> usize {
+    let mut score: usize = 0;
+    let mut multiplier: usize = 1;
+    if !p1.is_empty() {
+        for v in p1.iter().rev() {
+            score += *v as usize * multiplier;
+            multiplier += 1;
+        }
+    } else {
+        for v in p2.iter().rev() {
+            score += *v as usize * multiplier;
+            multiplier += 1;
+        }
     }
-    elements.push(String::from("p2"));
-    for el in p2 {
-        elements.push(el.to_string());
-    }
-    return elements.join(",");
+    score
 }
 
-fn part_1(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>) {
+fn part_1(mut p1: VecDeque<u8>, mut p2: VecDeque<u8>) {
     while p1.len() > 0 && p2.len() > 0 {
         let c1 = p1.pop_front().unwrap();
         let c2 = p2.pop_front().unwrap();
@@ -124,18 +100,5 @@ fn part_1(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>) {
         }
     }
 
-    let mut score = 0;
-    let mut multiplier = 1;
-    while p1.len() > 0 {
-        score += p1.pop_back().unwrap() * multiplier;
-        multiplier += 1;
-    }
-    while p2.len() > 0 {
-        score += p2.pop_back().unwrap() * multiplier;
-        multiplier += 1;
-    }
-    
-    println!("Part 1: {}", score);
+    println!("Part 1: {}", score(&p1, &p2));
 }
-
-

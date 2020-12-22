11ab6863df33adfe93ed2ca9dbdc4721755116b6
solipsis <davida3141@gmail.com>
2020-12-22 11:35:02 -0700

    day_22 part 2

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -18,53 +18,9 @@ fn main() {
 
     recursive_game(p1, p2, &mut memo, 1);
 
-    /*
-    let mut score = 0;
-    let mut multiplier = 1;
-    while p1.len() > 0 {
-        score += p1.pop_back().unwrap() * multiplier;
-        multiplier += 1;
-    }
-    while p2.len() > 0 {
-        score += p2.pop_back().unwrap() * multiplier;
-        multiplier += 1;
-    }
-    
-    println!("Part 1: {}", score);
-    */
-    /*
-    while p1.len() > 0 && p2.len() > 0 {
-        let c1 = p1.pop_front().unwrap();
-        let c2 = p2.pop_front().unwrap();
-
-        // recursive combat
-        if p1.len() >= c1 && p2.len() >= c2 {
-            // create new decks that are copies
-            let mut nd1: VecDeque<usize> = p1.clone();
-            while nd1.len() != c1 {
-                nd1.pop_back();
-            }
-            let mut nd2: VecDeque<usize> = p2.clone();
-            while nd2.len() != c2 {
-                nd2.pop_back();
-            }
-            recursive_game(p1.clone(), p2.clone(), &mut memo);
-        } else { // normal combat
-            if c1 > c2 {
-                p1.push_back(c1);
-                p1.push_back(c2);
-            } else {
-                p2.push_back(c2);
-                p2.push_back(c1);
-            }
-        }
-
-    }
-    */
-    
 }
 
-fn recursive_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, memo: &mut HashMap<String,bool>,  mut game: usize) -> bool {
+fn recursive_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, memo: &mut HashMap<String,bool>,  game: usize) -> bool {
 //    println!("start game: {}", game);
     
     // track all seen states in this particular game
@@ -72,14 +28,12 @@ fn recursive_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, memo: &mut H
     let mut sub_game = game;
 
     // hash the state to see if we have seen this substate before
-    /*
     let hash = blake3::hash(to_id(&p1, &p2).as_bytes()).to_hex().to_string();
     if let Some(prev) = memo.get(&hash) {
        // println!("hooray-cache?: {}", memo.len());
-        println!("cached game {} done: {}, cache-size: {}", game, *prev, memo.len());
+    //    println!("cached game {} done: {}, cache-size: {}", game, *prev, memo.len());
         return *prev;
     }
-    */
 
     //println!("{}", hash);
 
@@ -137,12 +91,10 @@ fn recursive_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>, memo: &mut H
             multiplier += 1;
         }
         println!("Part 2: {}", score);
-        panic!("done");
     }
-    
 
     // memoize
-    //memo.insert(hash, p1_wins);
+    memo.insert(hash, p1_wins);
     return p1_wins;
 }
 

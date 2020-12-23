285ced8d0dc8abe29bdbcc5fffeac01c439ce30a
solipsis <davida3141@gmail.com>
2020-12-23 12:55:41 -0700

    part 2

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,31 +1,25 @@
 
-const MAX_CUP: usize = 9;
+const MAX_CUP: usize = 1000000;
 const MIN_CUP: usize = 1;
-const ITERATIONS: usize = 100;
+const ITERATIONS: usize = 10000000;
 fn main() {
     //let input = vec![3,8,9,1,2,5,4,6,7];
     let input = vec![3,1,5,6,7,9,8,2,4];
 
     let mut next = vec![0; 1+MAX_CUP as usize]; // 1 indexed 
-    /*
     for x in 1..MAX_CUP+1 {
         next[x] = x+1;
     }
     next[MAX_CUP] = input[0];
-    */
 
     for (idx, v) in input.iter().enumerate() {
         if idx == input.len()-1 {
-            //next[*v as usize] = input.len()+1;
-            next[*v as usize] = input[0];
+            next[*v as usize] = input.len()+1;
+            //next[*v as usize] = input[0]; // swap this line for part 1
         } else {
             next[*v as usize] = input[idx+1];
         }
     }
-    println!("{:?}", &next);
-
-   // println!("{:?}", &next[..20]);
-   // print(&next[..20].to_vec());
 
     let mut current_cup = input[0];
     let mut take: Vec<usize> = vec![0; 3];
@@ -54,9 +48,8 @@ fn main() {
 
         current_cup = next[current_cup];
     }
-    print(&next);
+    //print(&next);
     println!("{} * {} = {}", next[1], next[next[1]], next[1] * next[next[1]]);
-//    println!("{:?}", &next[..20]);
 }
 
 fn print(next: &Vec<usize>) {
@@ -67,6 +60,5 @@ fn print(next: &Vec<usize>) {
         idx = next[idx];
     }
     println!("{:?}", out);
-    
 }
 

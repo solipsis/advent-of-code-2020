852ba0266d3aaec81d2132b451cfcaa57742d026
solipsis <davida3141@gmail.com>
2020-12-23 12:48:11 -0700

    still figuring out part 2

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,32 +1,36 @@
-use std::collections::VecDeque;
-use std::cmp;
 
 const MAX_CUP: usize = 9;
 const MIN_CUP: usize = 1;
+const ITERATIONS: usize = 100;
 fn main() {
     //let input = vec![3,8,9,1,2,5,4,6,7];
     let input = vec![3,1,5,6,7,9,8,2,4];
 
     let mut next = vec![0; 1+MAX_CUP as usize]; // 1 indexed 
-
+    /*
+    for x in 1..MAX_CUP+1 {
+        next[x] = x+1;
+    }
+    next[MAX_CUP] = input[0];
+    */
 
     for (idx, v) in input.iter().enumerate() {
         if idx == input.len()-1 {
+            //next[*v as usize] = input.len()+1;
             next[*v as usize] = input[0];
         } else {
             next[*v as usize] = input[idx+1];
         }
     }
-    //println!("{:?}", next);
+    println!("{:?}", &next);
+
+   // println!("{:?}", &next[..20]);
+   // print(&next[..20].to_vec());
 
     let mut current_cup = input[0];
     let mut take: Vec<usize> = vec![0; 3];
-    for _x in 0..100 {
-       // println!("-- Move {} -----", x+1);
-      //  println!("{:?}", vec![1,2,3,4,5,6,7,8,9]);
-      //  println!("{:?}", next[1..].to_vec());
-      //  print(&next);
-        
+    for _x in 0..ITERATIONS {
+
         // grab 3 to the right of current
         take[0] = next[current_cup];
         take[1] = next[take[0]];
@@ -48,21 +52,17 @@ fn main() {
         next[destination] = take[0];
         next[take[2]] = tmp;
 
-
-     //   println!("pickup: {:?}", take);
-     //   println!("destination: {:?}", destination);
-
-     //   println!("-----------------");
-
         current_cup = next[current_cup];
     }
     print(&next);
+    println!("{} * {} = {}", next[1], next[next[1]], next[1] * next[next[1]]);
+//    println!("{:?}", &next[..20]);
 }
 
 fn print(next: &Vec<usize>) {
     let mut idx = 1;
     let mut out = Vec::new();
-    for _x in 0..next.len()-1 {
+    for _x in 0..next.len() {
         out.push(idx);
         idx = next[idx];
     }
